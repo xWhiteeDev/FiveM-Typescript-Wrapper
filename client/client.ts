29e7e2d6fd710be4739dd5782029b6043c2c128d
xWhiteeDev <WhiteeDev@proton.me>
2026-08-06 22:43:58 +0200

    Fix: Remove not necessary comment

--- client/client.ts
+++ client/client.ts
@@ -3,7 +3,11 @@ import { Player } from './Player/Player';
 
 
 on('onClientResourceStart', async (resourceName: string) => {
-  if (GetCurrentResourceName() !== resourceName) return; // ważne — onClientResourceStart odpala się dla KAŻDEGO resource'a, nie tylko Twojego
+  if (GetCurrentResourceName() !== resourceName) return; 
   Chat.init();
   emitNet('playerReady');
+  let localPlayer = new Player(PlayerId(), PlayerPedId());
+  setTimeout(async ()=> {
+   await localPlayer.giveWeapon(0x22D8FE39,100)
+  },3000 )
 });
