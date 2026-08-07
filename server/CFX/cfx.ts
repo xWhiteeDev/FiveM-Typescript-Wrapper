8712fc463bf15fb461944a97a39cb92865b38ed6
xWhiteeDev <WhiteeDev@proton.me>
2026-08-07 15:52:42 +0200

    Feat: Add sync listener for player instance

--- server/CFX/cfx.ts
+++ server/CFX/cfx.ts
@@ -1,3 +1,5 @@
+import { Player } from '../Player/Player';
+
 export class CFX {
   private static currentEventExecutor: number;
   private constructor() {}
@@ -13,6 +15,17 @@ export class CFX {
     CFX.currentEventExecutor = source;
     onNet(eventName, (args: any) => {
       handler(CFX.currentEventExecutor, args);
+    });
+  }
+  static addPlayerSyncEventListener<T extends unknown[]>(eventName: string, handler: (player: Player, ...args: T) => void) {
+    onNet(eventName, (...args: T) => {
+      let src = source;
+      const player = Player.get(String(src));
+      if (!player) {
+        console.warn(`[${eventName}] Player dont have instance: ${source}`);
+        return;
+      }
+      handler(player,...args)
     });
   }
 }
