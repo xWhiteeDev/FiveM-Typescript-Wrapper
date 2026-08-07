2dcdb4d5b4b62f8908fb115b8f1a82c76b816d9a
xWhiteeDev <WhiteeDev@proton.me>
2026-08-07 15:50:49 +0200

    Feat: Remove unused test line

--- client/client.ts
+++ client/client.ts
@@ -1,13 +1,9 @@
 import { Chat } from './Chat/chat';
 import { Player } from './Player/Player';
 
-
 on('onClientResourceStart', async (resourceName: string) => {
-  if (GetCurrentResourceName() !== resourceName) return; 
+  if (GetCurrentResourceName() !== resourceName) return;
   Chat.init();
   emitNet('playerReady');
-  let localPlayer = new Player(PlayerId(), PlayerPedId());
-  setTimeout(async ()=> {
-   await localPlayer.giveWeapon(0x22D8FE39,100)
-  },3000 )
+  const localPlayer = new Player(PlayerId(), PlayerPedId());
 });
