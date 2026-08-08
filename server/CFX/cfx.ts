d0a8369adf3821bb6699634a474fb0116fa8cd86
xWhiteeDev <WhiteeDev@proton.me>
2026-08-08 22:56:52 +0200

    Fix:Race condition fix

--- server/CFX/cfx.ts
+++ server/CFX/cfx.ts
@@ -1,10 +1,14 @@
-//Version 1.0 
+//Version 1.0
 //It can't be ideal. It only needs to work actually.
-import { Player } from '../Player/Player';
-
+import type { Player } from '../Player/Player';
+import { CFXServerEvents } from '../types/Events';
 export class CFX {
-  private constructor() {}
-  static addEventListener(eventName: string, handler: Function) {
+  private static get:(src:string) => Player | undefined
+  static init (getPlayer:(src:string)=>Player | undefined) {
+    CFX.get = getPlayer
+    console.log('CFX Initialized')
+  }
+  static addEventListener(eventName: CFXServerEvents , handler: (player: Player, args?: any) => void) {
     if (!eventName) {
       console.error('[addEventListener]: eventName is not provided!');
       return;
@@ -14,14 +18,19 @@ export class CFX {
       return;
     }
     onNet(eventName, (args: any) => {
-      const src = source
-      handler(src, args);
+      const src = source;
+      const player = CFX.get(String(src));
+      if (!player) {
+        console.warn(`[${eventName}] Player dont have instance: ${source}`);
+        return;
+      }
+      handler(player, args);
     });
   }
   static addPlayerSyncEventListener<T extends unknown[]>(eventName: string, handler: (player: Player, ...args: T) => void) {
     onNet(eventName, (...args: T) => {
       let src = source;
-      const player = Player.get(String(src));
+      const player = CFX.get(String(src));
       if (!player) {
         console.warn(`[${eventName}] Player dont have instance: ${source}`);
         return;
