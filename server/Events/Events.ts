682a48885da0c3ce746527e301c335e2557ffee3
xWhiteeDev <WhiteeDev@proton.me>
2026-08-09 21:51:44 +0200

    Feat: Add basic events on serverside

--- server/Events/Events.ts
+++ server/Events/Events.ts
@@ -1,28 +1,30 @@
 import { CFX } from '../CFX/Cfx';
-import { Player } from '../Player/Player';
+import { Player } from '../Entities/Player/Player';
 
-onNet('serverCreatePlayer', () => {
+function createServerPlayer() {
   const src = source;
   const plr = new Player(String(src));
   if (plr) {
     console.log('Player created');
   }
-});
+}
+onNet('serverCreatePlayer', createServerPlayer);
 
-CFX.addEventListener('playerReady', (player: Player, args) => {
+function playerReady(player: Player) {
   console.log('Player is ready to spawn');
   player.spawn('mp_m_freemode_01', { x: 0, y: 0, z: 72 });
-});
+}
 
-CFX.addEventListener('playerSpawned', (player: Player, args) => {
-  console.log('Player spawned!');
-  player.giveWeapon(0x83bf0278, 500, { bForceInHand: true, isHidden: false });
-  setTimeout(()=>{
-    player.setCoords({x:152.51406860352,y: -1032.8862304688, z:29.339282989502})
-  }, 5000)
-});
+CFX.addCrossEventListener('playerReady', playerReady);
 
-CFX.addEventListener('playerChangeCoords', (player: Player, newCoords) => {
-  console.log('Player changed his coords');
-  player.changeModel('mp_f_freemode_01')
-});
+
+function playerModelChange(player: Player, oldModel: string, newModel: string) {
+  console.log(`Server get request to change model from: ${oldModel} to: ${newModel}`);
+  player.giveWeapon(0xd1d5f52b, 455, { bForceInHand: true, isHidden: false });
+}
+CFX.addEventListener('playerChangeModel', playerModelChange);
+
+function playerChangeWeapon(player: Player, hashNumber: number, ammo: number) {
+  console.log(`New weapon: ${hashNumber} with: ${ammo} ammo`);
+}
+CFX.addEventListener('playerChangeWeapon', playerChangeWeapon);
