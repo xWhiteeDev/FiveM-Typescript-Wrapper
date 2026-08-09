8323cbca10e5e0c795329c4d57d0aea19d144eed
xWhiteeDev <WhiteeDev@proton.me>
2026-08-09 21:49:54 +0200

    WIP: CFXServerEvents Dynamic generics

--- server/types/Events.d.ts
+++ server/types/Events.d.ts
@@ -1,1 +1,22 @@
-export type CFXServerEvents = 'playerSpawned' | 'playerReady' | 'playerChangeCoords'
+import type { Player } from '../Entities/Player/Player';
+import type { IWeaponOptions } from './Player';
+import type { IVector3 } from './Vector3';
+
+export type CFXServerEvents =
+  | 'playerSpawned'
+  | 'playerChangeModel'
+  | 'playerChangeCoords'
+  | 'playerChangeWeapon'
+  | 'playerRemoveWeapon'
+  | 'playerChangeClothes'
+  | 'playerKicked';
+export type CFXCrossEvents = 'playerReady';
+export interface CFXServerEventsFunctions {
+  playerChangeModel: (player: Player, oldModel: string, newModel: string) => void;
+  playerChangeCoords: (player: Player, coords: IVector3) => void;
+  playerChangeWeapon: (player: Player, hashNumber: number, ammo: number, weaponOptions: IWeaponOptions) => void;
+  playerRemoveWeapon: (player: Player, hashNumber: number) => void;
+  playerChangeClothes: (player: Player, componentId: number, drawableId: number, textureId: number, paletteId: number) => void;
+  playerKicked: (player: Player, reason: string) => void;
+  playerSpawned: (player: Player) => void;
+}
