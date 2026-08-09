914fd4c033871766b3303a9e4d4738834e7dcc61
xWhiteeDev <WhiteeDev@proton.me>
2026-08-09 21:47:23 +0200

    Feat: Add modules loader

--- client/client.ts
+++ client/client.ts
@@ -1,10 +1,4 @@
-import { Chat } from './Chat/chat';
-import { Player } from './Player/Player';
-
-on('onClientResourceStart', async (resourceName: string) => {
-  if (GetCurrentResourceName() !== resourceName) return;
-  Chat.init();
-  emitNet('serverCreatePlayer');
-  emitNet('playerReady');
-  new Player(PlayerId(), PlayerPedId());
-});
+import './Entities/Player/Player';
+import './CFX/cfx'
+import './Events/clientEvents';
+import './Events/playerEvents'
