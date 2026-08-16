edca1bf059dda19d7d73670b3ffe85f319b58f8c
xWhiteeDev <WhiteeDev@proton.me>
2026-08-16 22:41:55 +0200

    Feat: Delete un-usable

--- client/Math/Quaternion.ts
+++
@@ -1,5 +0,0 @@
-export class Quaternion {
-    constructor (public x:number,public y:number,public z:number,public w:number) {
-        
-    }
-}
--- client/Math/Vector3.ts
+++
@@ -1,9 +0,0 @@
-export class Vector3 {
-
-    constructor (public x:number,public y:number, public z:number) {
-
-    }
-    static fromArray(arr:number[]) {
-        return {x:arr[0], y:arr[1], z:arr[2]}
-    }
-}
