e18a6a397de373837d5b7bbf929fa50fb1716544
xWhiteeDev <WhiteeDev@proton.me>
2026-08-16 20:22:21 +0200

    Fix: Esbuild, package.json

--- esbuild.config.js
+++ esbuild.config.js
@@ -5,6 +5,7 @@ import {
 import esbuild from 'esbuild'
 const serverEntries = glob.sync('server/**/*.ts')
 const clientEntries = glob.sync('client/**/*.ts')
+const sharedEntries = glob.sync('shared/**/*.ts')
 
 await Promise.all([esbuild.build({
     bundle: false,
@@ -20,4 +21,11 @@ await Promise.all([esbuild.build({
     outbase: 'server',
     minify: false,
     platform: 'node'
+}), esbuild.build({
+    bundle: false,
+    entryPoints: [...sharedEntries],
+    outdir: './dist/shared',
+    outbase: 'shared',
+    minify: false,
+    platform: 'neutral'
 })])
