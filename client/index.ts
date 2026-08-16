c23d51dbf8c48215fd5ae003a181af47470f5a38
xWhiteeDev <WhiteeDev@proton.me>
2026-08-16 22:40:03 +0200

    Feat: Add Checkpoint and blip module

--- client/index.ts
+++ client/index.ts
@@ -2,9 +2,10 @@ export { CFX } from './CFX/cfx';
 export { LocalPlayer } from './Entities/LocalPlayer';
 export { Vehicle } from './Entities/Vehicle';
 export { Utils } from './Utils/Utils';
-export { Blip } from './Entities/Blip';
+export { Blip } from './World/Blip';
 export { Web } from './Web/Web';
 export { BaseEntity } from './Entities/BaseEntity';
 export { Entity } from './Entities/Entity';
 export { Ped } from './Entities/Ped';
-export { WorldObject } from './Entities/WorldObject';
+export { WorldObject } from './World/WorldObject';
+export {Checkpoint} from './World/Checkpoint'
