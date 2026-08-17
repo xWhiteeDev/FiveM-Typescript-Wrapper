64f461cfe9ee9f1ab32b6d7ad4fd5d49a0788ad8
xWhiteeDev <WhiteeDev@proton.me>
2026-08-17 13:37:42 +0200

    Feat: Implement exports for new modules

--- client/index.ts
+++ client/index.ts
@@ -1,11 +1,30 @@
 export { CFX } from './CFX/cfx';
 export { LocalPlayer } from './Entities/LocalPlayer';
 export { Vehicle } from './Entities/Vehicle';
-export { Utils } from './Utils/Utils';
-export { Blip } from './World/Blip';
-export { Web } from './Web/Web';
 export { BaseEntity } from './Entities/BaseEntity';
 export { Entity } from './Entities/Entity';
 export { Ped } from './Entities/Ped';
+
 export { WorldObject } from './World/WorldObject';
-export {Checkpoint} from './World/Checkpoint'
+export { Checkpoint } from './World/Checkpoint';
+export { Rope } from './World/Rope';
+export { Blip } from './World/Blip';
+
+export { Utils } from './Utils/Utils';
+export { Web } from './Web/Web';
+
+export { AnimPostFx } from './Graphics/AnimPostFx';
+export { BinkMovie } from './Graphics/Bink';
+export { DebugDraw } from './Graphics/DebugDraw';
+export { Decal } from './Graphics/Decals';
+export { Graphics } from './Graphics/Graphics';
+export { HUD } from './Graphics/HUD';
+export { Lights } from './Graphics/Lights';
+export { Minimap } from './Graphics/Minimap';
+export { ParticleFx } from './Graphics/Particlefx';
+export { PhotoMode } from './Graphics/Photomode';
+export { Scaleform } from './Graphics/Scaleform';
+export { ScreenEffects } from './Graphics/ScreenEffects';
+export { Timecycle } from './Graphics/TimeCycle';
+export { TvChannel } from './Graphics/TvChannel';
+ 
